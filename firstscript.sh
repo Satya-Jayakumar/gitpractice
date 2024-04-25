b98430b2968e295edf49251f9f411aae94cd77eb
Satya <satyajayakumar135@gmail.com>
2024-04-25 22:01:35 +0530

    3 basic files added

---
+++ firstscript.sh
@@ -0,0 +1,21 @@
+
+
+#!/bin/bash
+
+##############
+
+# Description : Test Script 
+# Version : V1
+
+#############
+
+
+#set -x
+
+
+echo "My name is Satya" 
+
+
+ 
+
+
---
+++ nodehealth.sh
@@ -0,0 +1,25 @@
+
+
+
+#!/bin/bash
+
+###########
+
+
+# Author: Satya
+# Date:25/04/24
+# This is node health script
+# Version: V1
+###########
+
+set -x
+
+set -e
+set -o pipefail
+
+df -h
+
+free -g
+
+
+npro
